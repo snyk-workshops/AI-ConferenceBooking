41e500dc174d5990f25d84b2d8c38e3adc2d9fbc
Micah Silverman <micah@afitnerd.com>
2024-07-15 09:18:32 -0400

    Added some whitespace in search code

--- src/main/java/org/workshop/aiconferencebooking/repository/SearchRepository.java
+++ src/main/java/org/workshop/aiconferencebooking/repository/SearchRepository.java
@@ -21,8 +21,10 @@ public class SearchRepository {
     public List<Talk> searchTalk(String input) {
         // lowercase the input
         var lowerInput = input.toLowerCase();
+
         // create a native query to search for talks joined with the person table with the input in the description, title or speaker username
         var query = em.createNativeQuery("select t.* from Talk t join Person p on t.speaker_id = p.id where lower(t.description) like '%" + lowerInput + "%' OR lower(t.title) like '%" + lowerInput + "%' OR lower(p.username) like '%" + lowerInput + "%'", Talk.class);
+
         // execute the query
         var talks = (List<Talk>) query.getResultList();
         // return the result
