8b7d00c63b21f624f7bd1880656afb655710e8bd
Micah Silverman <micah@afitnerd.com>
2024-12-10 22:11:16 -0500

    Ensure that OldUploadController.java has the insecure code

--- src/main/java/org/workshop/aiconferencebooking/controller/OldUploadController.java
+++ src/main/java/org/workshop/aiconferencebooking/controller/OldUploadController.java
@@ -20,14 +20,7 @@ public class OldUploadController {
     public String uploadImage(Model model, MultipartFile file, Principal principal) throws IOException {
         var name = file.getOriginalFilename().replace(" ", "_");
         var fileNameAndPath = Paths.get(UPLOAD_DIRECTORY + File.separator + name);
-
-        var normalizedPath = fileNameAndPath.normalize();
-        if (!normalizedPath.startsWith(Paths.get(UPLOAD_DIRECTORY).normalize())) {
-            model.addAttribute("message", "ERROR");
-            return "person/upload";
-        }
-
-        Files.write(normalizedPath, file.getBytes());
+        Files.write(fileNameAndPath, file.getBytes());
         model.addAttribute("msg", "Uploaded images: " + name);
 
         if (principal == null) {
